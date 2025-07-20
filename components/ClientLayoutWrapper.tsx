cbfd0d7cf80762905f34cc504e8de3e7a74bec26
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-07-20 21:47:05 +0530

    update

--- components/ClientLayoutWrapper.tsx
+++ components/ClientLayoutWrapper.tsx
@@ -1,18 +1,15 @@
-'use client'
+'use client';
 
-import { usePathname } from 'next/navigation'
-import Navbar from './navbar'
-import Footer from './footer/page'
+import { usePathname } from 'next/navigation';
+import Navbar from './navbar';
+import Footer from './footer/page';
 
 export function ClientLayoutWrapper({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname()
+  const pathname = usePathname();
 
-<<<<<<< HEAD
-  const hideLayout = ['/login', '/register', '/forgotpassword','/verifyemail']
-=======
-  const hideLayout = ['/login', '/register', '/forgotpassword', '/verifyemail']
->>>>>>> aaec873f1e4c20249ac11ff3e8b6ba8b908b773a
-  const shouldHide = hideLayout.includes(pathname)
+  // List of routes that should NOT show Navbar and Footer
+  const hideLayout = ['/login', '/register', '/forgotpassword', '/verifyemail'];
+  const shouldHide = hideLayout.includes(pathname);
 
   return (
     <>
@@ -20,5 +17,5 @@ export function ClientLayoutWrapper({ children }: { children: React.ReactNode })
       <main>{children}</main>
       {!shouldHide && <Footer />}
     </>
-  )
+  );
 }
