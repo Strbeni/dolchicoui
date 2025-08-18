98ab42ccf95fdaab74b9ed33c81946e8ff35b625
Abhi773925 <rockabhisheksingh778189@gmail.com>
2025-08-19 00:49:58 +0530

    issue fixed

--- components/ui/navbar-5.tsx
+++ components/ui/navbar-5.tsx
@@ -616,7 +616,7 @@ export const Navbar5 = () => {
                         className="block px-4 py-2 hover:bg-gray-100"
                         onClick={() => setUserMenuOpen(false)}
                       >
-                        Profileaa
+                        Profile
                       </Link>
                       <Link
                         href="/profile/orderHistory"
